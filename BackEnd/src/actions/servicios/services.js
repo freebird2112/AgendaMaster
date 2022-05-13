868c59dec524f4b93668f2ad107597be4c0b22e2
marii4 <maria.riffo@sansano.usm.cl>
2022-05-13 18:18:34 -0400

    new first commit

---
+++ BackEnd/src/actions/servicios/services.js
@@ -0,0 +1,120 @@
+const servicesData = require('../../data/servicios.json')
+
+exports.getServices = (showPayment) =>{
+    const bool = showPayment
+    const company = []
+    servicesData.services.forEach((element)=> {
+        obj = {}
+        obj.serviceName = element.serviceName
+        obj.horario1 = element.horario1
+        obj.horario2 = element.horario2
+        if(!bool){
+            company.push(obj)
+        }else{
+            obj.paymentType = element.paymentType
+            obj.createdAt = element.createdAt
+            company.push(obj)
+        }       
+    })
+    return company
+}
+
+exports.searchByName = (nombre) => {
+    const object = {
+        Name : nombre.serviceName
+    } 
+    console.log(object.Name) //esto nos tira false    
+
+    let response = []
+    let company = this.getServices(false)    //revisado 
+
+    company.forEach((element) => {
+        if(element.serviceName == object.Name){  //no entra,error aca
+            response.push(element)
+        }
+        else {
+            console.log("No entro cotito")
+        }
+    })
+    return response
+}
+
+
+
+/*exports.getService = (ctx) =>{
+
+    servicios.json.name.find({name:ctx}
+        },function(err, doc) {
+            const company = []
+            servicesData.services.forEach((element)=> {
+                obj = {}
+                obj.horario1 = element.horario1
+                obj.horario2 = element.horario2
+                if(!bool){
+                    company.push(obj)
+                }else{
+                    obj.paymentType = element.paymentType
+                    obj.createdAt = element.createdAt
+                    company.push(obj)
+                }       
+            })
+            return company
+        });
+}*/
+    
+
+
+exports.addUsers = (userData) => {
+    const user = {
+        name: userData.name,
+        email: userData.email,
+        horario: userData.horario
+    }
+    users.push(user)
+}
+
+
+
+/*
+exports.searchByNameOrLocation = (search) => {
+    const object = {
+        searchCompany : search.search
+    }
+    let response = []
+    let company = this.getCompanies(false)
+
+    company.forEach((element) => {
+        if(element.companyName == object.searchCompany){
+            response.push(element)
+        }
+    })
+    return response
+}
+
+exports.searchByPayment = (payment, order) => {
+    let response = []
+    let company = this.getCompanies(true)
+
+    company.forEach((element) => {
+        if(element.paymentType == payment){
+            response.push(element)
+        }
+    })
+
+    if(order == "asc"){
+        response.sort((a,b) =>{
+            a = new Date(a.createdAt)
+            b = new Date(b.createdAt)
+            return a - b;
+        })
+    }else if(order == 'dsc'){
+        response.sort((a,b) => {
+            a = new Date(a.createdAt)
+            b = new Date(b.createdAt)
+            return b - a;
+        })
+    }
+
+    return response
+}
+*/
---
+++ BackEnd/src/routes/index.js
@@ -0,0 +1,14 @@
+const Router = require('koa-router')
+const companies = require('./Services/services.route')
+
+const router = new Router()
+
+router.get('/service', companies.getAllServices)
+
+router.post('/service', companies.postService)
+
+//router.post('/companies', companies.searchByNameOrLocation)
+
+//router.get('/companies/byptype/:paymentType/:order', companies.searchByPaymentType)
+
+module.exports = router
